7b021f1b184aade6b45dbb61294a30dbe8d4ecc2
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-06-23 02:08:58 +0300

    Fix autodiscover on 1.7

--- tests/test_project/urls.py
+++ tests/test_project/urls.py
@@ -3,7 +3,9 @@ try:
 except ImportError:
     from django.conf.urls.defaults import patterns, handler404, handler500, include, url
 from django.contrib import admin
-admin.autodiscover()
+import django
+if django.VERSION < (1, 7):
+    admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
