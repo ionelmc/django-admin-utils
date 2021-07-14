cdfd834c01c873a7c41f317d2c7aba8dd74ecc5d
Ionel Cristian Mărieș <contact@ionelmc.ro>
2021-07-14 15:33:53 +0300

    Bump to min tested version.

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
     install_requires=[
-        'Django>=1.1',
+        'Django>=1.11',
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
