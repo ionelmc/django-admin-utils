e04231f03974a348ada745cd61aee7baa78d22b0
Ionel Cristian Mărieș <contact@ionelmc.ro>
2021-07-18 19:14:17 +0300

    Forgot to import it.

--- src/admin_utils/__init__.py
+++ src/admin_utils/__init__.py
@@ -1,6 +1,12 @@
-from .brief import inline  # noqa
+from .brief import inline
 from .brief import register
-from .mock import make_admin_class  # noqa
+from .mock import make_admin_class
+from .mock import register_view
 
 __version__ = '2.0.0'
-__all__ = ['inline', 'register', 'make_admin_class']
+__all__ = [
+    'inline',
+    'make_admin_class',
+    'register',
+    'register_view',
+]
