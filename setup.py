d1c1d64a6d97f3589746c06352b21500d234209d
Ionel Cristian Mărieș <contact@ionelmc.ro>
2021-07-19 16:48:23 +0300

    Bump version: 2.0.3 → 2.0.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='django-admin-utils',
-    version='2.0.3',
+    version='2.0.4',
     license='BSD-2-Clause',
     description='Utility code and patterns.',
     long_description='%s\n%s' % (
